9ef28760acf995bcd80c84d07b3f10afa11d64a6
Kenny <kenji.maddela01@gmail.com>
2022-03-18 21:50:37 -0400

    add functions for buttons

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let gridContainer = document.querySelector('.grid');
+let blackButton = document.querySelector('.black');
+let grayButton = document.querySelector('.gray');
+let rgbButton = document.querySelector('.rgb');
+let eraserButton = document.querySelector('.eraser');
+let clearButton = document.querySelector('.clear');
+let resizeButton = document.querySelector('.resize');
 
 function createBox(rows, columns){
 
@@ -8,11 +14,101 @@ function createBox(rows, columns){
         gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         gridContainer.appendChild(box).classList.add('gridBox');
     }
-
-    
-
 }
 
-createBox(16, 16);
+function blackColor(){
+    let gridBoxes = document.querySelectorAll('.gridBox');
+    blackButton.addEventListener('click', () => {
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', () =>{
+            gridBox.style.background = 'black';
+        }))
+    })
+}
 
-console.log('hello');
+function grayColor(){
+    let gridBoxes = document.querySelectorAll('.gridBox');
+    grayButton.addEventListener('click', () => {
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', () =>{
+            let rgb = Math.floor(Math.random() * 255);
+            console.log('clicked');
+            gridBox.style.background = `rgb(${rgb}, ${rgb}, ${rgb})`;
+        }))
+    })
+}
+
+function rgbColor(){
+    let gridBoxes = document.querySelectorAll('.gridBox');
+    rgbButton.addEventListener('click', () => {
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', () =>{
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+
+            gridBox.style.background = `rgb(${r}, ${g}, ${b})`;
+
+        }))
+    })
+}
+
+function eraser(){
+    let gridBoxes = document.querySelectorAll('.gridBox');
+    eraserButton.addEventListener('click', () => {
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', () =>{
+            gridBox.style.background = 'white';
+        }))
+    })
+}
+
+function clear(){
+    let gridBoxes = document.querySelectorAll('.gridBox');
+    clearButton.addEventListener('click', () => {
+        for (let i = 0; i < gridBoxes.length; i++){
+            gridBoxes[i].style.background = `white`;
+        }
+    })
+}
+
+function resize(){
+    resizeButton.addEventListener('click', () => {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
+          }
+        gridSize = slider.value;
+        reset();
+    })
+}
+
+function reset(){
+    createBox(gridSize, gridSize);
+    blackColor();
+    grayColor();
+    eraser();
+    clear();
+    rgbColor();
+    resize();
+}
+
+
+/*credits to w3school*/
+var slider = document.getElementById("myRange");
+var output0 = document.getElementById("row-value");
+var output1 = document.getElementById("column-value");
+
+let gridSize = slider.value;
+
+output0.textContent = gridSize;
+output1.textContent = gridSize;
+
+slider.oninput = function() {
+  output0.innerHTML = this.value;
+  output1.innerHTML = this.value;
+}
+
+
+createBox(gridSize, gridSize);
+blackColor();
+grayColor();
+eraser();
+clear();
+rgbColor();
+resize();
